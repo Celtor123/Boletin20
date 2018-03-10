11e41f3835471f6e3d02bf030604c71291500295
Juan <Juan@DESKTOP-J5PU6RU>
2018-03-10 23:57:52 +0100

    Probando.

--- src/boletin20/Boletin20.java
+++ src/boletin20/Boletin20.java
@@ -1,10 +1,41 @@
 
 package boletin20;
 
-public class Boletin20 {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
 
+public class Boletin20 {
+    File ficheiro= new File("a.txt");
+    ArrayList <Libro> liston;
+
+    public Boletin20() {
+        this.liston = new ArrayList<>();
+    }
+    public void engadir(){
+        try{
+           String z= JOptionPane.showInputDialog("escriba o nume");
+           String x= JOptionPane.showInputDialog("escriba o autor");
+            String zo= JOptionPane.showInputDialog("escriba o precio");
+           double t=Double.parseDouble(zo);
+           String e= JOptionPane.showInputDialog("escriba o nume");
+           int r=Integer.parseInt(e);
+          
+           liston.add(new Libro(z,x,t,r));
+           PrintWriter a= new PrintWriter(ficheiro); 
+           liston.forEach((nome) -> {
+               a.println(nome);
+            }); 
+        }
+        catch(FileNotFoundException t){
+            System.out.println("obxecto non encontrado"+ficheiro);
+        }    
+    }    
     public static void main(String[] args) {
-      
+      Boletin20 a= new Boletin20();
+      a.engadir();
     }
     
 }
